f24dce03d82f17b5417278d730c3050ba2f94240
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-03-05 21:04:47 +0200

    First commit

---
+++ src/learning/vladdubceac/facade/Projector.java
@@ -0,0 +1,32 @@
+package learning.vladdubceac.facade;
+
+public class Projector {
+    String description;
+    DvdPlayer dvdPlayer;
+
+    public Projector(String description, DvdPlayer dvdPlayer) {
+        this.description = description;
+        this.dvdPlayer = dvdPlayer;
+    }
+
+    public void on(){
+        System.out.println(description+" on");
+    }
+
+    public void off(){
+        System.out.println(description+" off");
+    }
+
+    public void wideScreenMode(){
+        System.out.println(description+" in widescreen mode (16x9 aspect ratio)");
+    }
+
+    public void tvMode(){
+        System.out.println(description+" in TV mode (4x3 aspect ratio)");
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
---
+++ src/learning/vladdubceac/facade/Screen.java
@@ -0,0 +1,22 @@
+package learning.vladdubceac.facade;
+
+public class Screen {
+    String description;
+
+    public Screen(String description) {
+        this.description = description;
+    }
+
+    public void up(){
+        System.out.println(description+" going up");
+    }
+
+    public void down(){
+        System.out.println(description+" going down");
+    }
+
+    @Override
+    public String toString() {
+        return description ;
+    }
+}
